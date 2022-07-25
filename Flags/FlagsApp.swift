55ff02b3401da571a468f452d98cc5f6d168e574
daria-andrioaie <daria.andrioaie@gmail.com>
2022-07-25 14:46:01 +0300

    First commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Daria Andrioaie on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
